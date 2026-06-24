1548224fc595e6d9debe261ecf5e1bfeef427725
agnetic-ai <bangkithari18@gmail.com>
2026-06-24 11:38:53 +0700

    replace template

--- index.php
+++ index.php
@@ -5,27 +5,21 @@ use Dompdf\Dompdf;
 use Dompdf\Options;
 
 $options = new Options();
-$options->set('isRemoteEnabled', false);
+$options->set('isRemoteEnabled', true);
 $options->set('isHtml5ParserEnabled', true);
-$options->set('defaultMediaType', 'screen');
+
+$options->set('defaultMediaType', 'print');
 $options->set('defaultFont', 'Calibri');
 
 $dompdf = new Dompdf($options);
 
-$html = file_get_contents('riplay_fixed.html');
-
-// Fix: border-collapse: collapse → separate (DomPDF page-break compatibility)
-$html = str_replace(
-    'border-collapse: collapse;',
-    'border-collapse: separate; border-spacing: 0;',
-    $html
-);
+// $html = file_get_contents('CPP-USD.html');
+$html = file_get_contents('CPP-IDR.html');
 
 $dompdf->loadHtml($html);
 $dompdf->setPaper('A4', 'portrait');
 $dompdf->render();
 
-// Output to browser
 header('Content-Type: application/pdf');
-header('Content-Disposition: inline; filename="riplay_100_percent.pdf"');
+header('Content-Disposition: inline; filename="template-gemini.pdf"');
 echo $dompdf->output();
